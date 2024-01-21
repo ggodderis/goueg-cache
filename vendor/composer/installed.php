7b8df4fca804966b5177e4c82c9a13c8864e49ca
ggodderis <goueg@orange.fr>
2024-01-21 14:48:38 +0100

    add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'goueg/cache',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '94e39d4af7fcd5020d5aa9f155fcd75d69e1962a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'goueg/cache' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '94e39d4af7fcd5020d5aa9f155fcd75d69e1962a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
